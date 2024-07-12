bd89d4105216fd71358a2e0de36ad43df61b2f5f
ErandaMadusanka <emsamarasinghe@gmail.com>
2024-07-12 06:56:19 +0530

    add files and business logics

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { MealsModule } from './meals/meals.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, MealsModule],
 })
 export class AppModule {}
